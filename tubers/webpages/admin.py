cfbc37aae125c9d87088c9615a1e9404a95fdc69
Afsan <khanafsaan11@gmail.com>
2021-06-13 01:18:09 +0530

    day 4

--- tubers/webpages/admin.py
+++ tubers/webpages/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Slider
+from .models import Slider , Team
+from django.utils.html import format_html
 # Register your models here.
 
+
+class TeamAdmin(admin.ModelAdmin):
+
+
+    def my_photo(self , objects):
+        return format_html('<img src={} width=40>'.format(objects.photo.url))
+
+
+    list_display = ('id' , 'my_photo' , 'first_name' , 'role' , 'create_data') # how you want to list on the admin panel
+    list_display_links = ('first_name' , )  # make column a clickable
+    search_fields = ('first_name', )  # activate the search key
+    list_filter = ('role',) # add filter option
+
 admin.site.register(Slider)
+admin.site.register(Team , TeamAdmin)
+
--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -7,7 +7,21 @@ class Slider(models.Model):
     button_text = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='media/slider/%Y/')
     created_date = models.DateTimeField(auto_now=True)
-
+    link = models.CharField( max_length=255)
 
     def __str__(self):
         return self.subtitle
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    fb_link = models.CharField(max_length=255)
+    insta_link = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to = "media/team/%Y/%m")
+    create_data = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.first_name
+
